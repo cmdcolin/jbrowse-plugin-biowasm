bf07590a35b8ec29076130a8c1e96c30b7c9da9c
Colin <colin.diesh@gmail.com>
2021-03-04 21:08:24 -0700

    Copy almost all of mismatch parser

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,12 @@ import {
   ConfigurationSchema,
 } from '@jbrowse/core/configuration'
 import { AnyConfigurationModel } from '@jbrowse/core/configuration/configurationSchema'
+import { BaseFeatureDataAdapter } from '@jbrowse/core/data_adapters/BaseAdapter'
 import { types } from 'mobx-state-tree'
 import { Aioli } from '@biowasm/aioli'
 import { version } from '../package.json'
 
-function parseCigar(cigar: string) {
-  return (cigar || '').split(/([MIDNSHPX=])/)
-}
+import { parseCigar, getMismatches } from './MismatchParser'
 
 function locstr(query: Region) {
   return `${query.refName}:${query.start}-${query.end}`
@@ -43,112 +42,127 @@ const configSchema = ConfigurationSchema(
   { explicitlyTyped: true },
 )
 
-function getAdapterClass(pluginManager: PluginManager) {
-  const { jbrequire } = pluginManager
-  const { BaseFeatureDataAdapter } = jbrequire(
-    '@jbrowse/core/data_adapters/BaseAdapter',
-  )
+class AdapterClass extends BaseFeatureDataAdapter {
+  private config: AnyConfigurationModel
+  private samtools: any
+  private mountedFiles: any
 
-  return class AdapterClass extends BaseFeatureDataAdapter {
-    private config: AnyConfigurationModel
-    private samtools: any
-    private mountedFiles: any
+  constructor(config: AnyConfigurationModel) {
+    super(config)
+    this.config = config
+  }
 
-    constructor(config: AnyConfigurationModel) {
-      super(config)
-      this.config = config
+  // setupSamtools in a way where multiple invocations gets a cached version
+  async setupSamtools() {
+    if (!this.samtools) {
+      const samtools = new Aioli('samtools/1.10')
+      this.samtools = new Promise(async resolve => {
+        await samtools.init()
+        resolve(samtools)
+      })
     }
+    return this.samtools
+  }
 
-    // setupSamtools in a way where multiple invocations gets a cached version
-    async setupSamtools() {
-      if (!this.samtools) {
-        const samtools = new Aioli('samtools/1.10')
-        this.samtools = new Promise(async resolve => {
-          await samtools.init()
-          resolve(samtools)
-        })
-      }
-      return this.samtools
+  // setup everything in a way where multiple invocations gets a cached version
+  async setup() {
+    await this.setupSamtools()
+    if (!this.mountedFiles) {
+      // set it to not undefined to start
+      this.mountedFiles = Promise.all([
+        Aioli.mount(readConfObject(this.config, 'bamLocation').uri),
+        Aioli.mount(readConfObject(this.config, ['index', 'location']).uri),
+      ])
     }
+    return this.mountedFiles
+  }
 
-    // setup everything in a way where multiple invocations gets a cached version
-    async setup() {
-      await this.setupSamtools()
-      if (!this.mountedFiles) {
-        // set it to not undefined to start
-        this.mountedFiles = Promise.all([
-          Aioli.mount(readConfObject(this.config, 'bamLocation').uri),
-          Aioli.mount(readConfObject(this.config, ['index', 'location']).uri),
-        ])
-      }
-      return this.mountedFiles
+  public async getRefNames(_: BaseOptions = {}) {
+    const samtools = await this.setupSamtools()
+    const [bamFile] = await this.setup()
+    const { stdout, stderr } = await samtools.exec(`view ${bamFile.path} -H`)
+    if (stdout === '' && stderr !== '') {
+      throw new Error(stderr)
     }
+    return (stdout as string)
+      .split('\n')
+      .filter(line => line.startsWith('@SQ'))
+      .map(line => {
+        const [, SN] = line.split('\t')
+        return SN.replace(/^SN:/, '')
+      })
+  }
 
-    public async getRefNames(_: BaseOptions = {}) {
+  public getFeatures(query: Region, opts: BaseOptions = {}) {
+    return ObservableCreate<Feature>(async observer => {
       const samtools = await this.setupSamtools()
       const [bamFile] = await this.setup()
-      const { stdout, stderr } = await samtools.exec(`view ${bamFile.path} -H`)
-      if (stdout === '' && stderr !== '') {
-        throw new Error(stderr)
+      const readData = await samtools.exec(
+        `view ${bamFile.path} ${locstr(query)}`,
+      )
+
+      if (readData && !readData.stdout) {
+        throw new Error(readData.stderr)
       }
-      return (stdout as string)
+
+      ;(readData.stdout as string)
         .split('\n')
-        .filter(line => line.startsWith('@SQ'))
-        .map(line => {
-          const [, SN] = line.split('\t')
-          return SN.replace(/^SN:/, '')
-        })
-    }
-
-    public getFeatures(query: Region, opts: BaseOptions = {}) {
-      return ObservableCreate<Feature>(async observer => {
-        const samtools = await this.setupSamtools()
-        const [bamFile] = await this.setup()
-        const readData = await samtools.exec(
-          `view ${bamFile.path} ${locstr(query)}`,
-        )
-
-        if (readData && !readData.stdout) {
-          throw new Error(readData.stderr)
-        }
-
-        ;(readData.stdout as string)
-          .split('\n')
-          .filter(f => !!f)
-          .forEach(row => {
-            const [, flagStr, refName, startStr, , CIGAR] = row.split('\t')
-            const start = +startStr
-            const flags = +flagStr
-            const cigarOps = parseCigar(CIGAR)
-            let length = 0
-            for (let i = 0; i < cigarOps.length; i += 2) {
-              const len = +cigarOps[i]
-              const op = cigarOps[i + 1]
-              if (op !== 'I' && op !== 'S' && op !== 'H') {
-                length += len || 0
-              }
+        .filter(f => !!f)
+        .forEach(row => {
+          const [
+            ,
+            flagStr,
+            refName,
+            startStr,
+            ,
+            CIGAR,
+            RNEXT,
+            PNEXT,
+            TLEN,
+            seq,
+            qual,
+            ...tags
+          ] = row.split('\t')
+          let MD = tags.find(tag => tag.startsWith('MD'))
+          if (MD) MD = MD.replace('MD:', '')
+          const start = +startStr
+          const flags = +flagStr
+          const cigarOps = parseCigar(CIGAR)
+          let length = 0
+          for (let i = 0; i < cigarOps.length; i += 2) {
+            const len = +cigarOps[i]
+            const op = cigarOps[i + 1]
+            if (op !== 'I' && op !== 'S' && op !== 'H') {
+              length += len || 0
             }
+          }
 
-            observer.next(
-              new SimpleFeature({
-                refName,
-                start,
-                end: start + length,
-                strand: flags & 16 ? -1 : 1,
-                mismatches: [],
-                CIGAR,
-                flags,
-                uniqueId: row,
-              }),
-            )
-          })
+          observer.next(
+            new SimpleFeature({
+              refName,
+              start,
+              end: start + length,
+              strand: flags & 16 ? -1 : 1,
+              mismatches: getMismatches(CIGAR, MD || '', seq),
+              CIGAR,
+              MD,
+              RNEXT,
+              PNEXT,
+              TLEN,
+              qual,
+              seq,
+              flags,
+              tags,
+              uniqueId: row,
+            }),
+          )
+        })
 
-        observer.complete()
-      }, opts.signal)
-    }
-
-    public freeResources(/* { region } */) {}
+      observer.complete()
+    }, opts.signal)
   }
+
+  public freeResources(/* { region } */) {}
 }
 
 export default class extends Plugin {
@@ -160,7 +174,6 @@ export default class extends Plugin {
     const AdapterType = jbrequire(
       '@jbrowse/core/pluggableElementTypes/AdapterType',
     )
-    const AdapterClass = getAdapterClass(pluginManager)
     pluginManager.addAdapterType(() => {
       return new AdapterType({
         name: 'BiowasmAdapter',
